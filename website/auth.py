73e95fa87659f10a831d5b05606816027a3359b4
Camtayou <117365620+Camtayou@users.noreply.github.com>
2022-12-18 00:16:42 +0100

    Delete website directory

--- website/auth.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-auth = Blueprint('auth', __name__)
-
-
-@auth.route('/')
-def home():
-    return render_template("appartayou.html")
-    #return "<h1> Test</h1>"
