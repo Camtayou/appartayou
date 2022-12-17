a87f0c9ea37f6523d12106ed3d20b500619f797e
Camtayou <117365620+Camtayou@users.noreply.github.com>
2022-12-18 00:04:09 +0100

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -5,5 +5,5 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/')
 def home():
-    return render_template("appartayou.html")
+    return render_template("index.html")
     #return "<h1> Test</h1>"
